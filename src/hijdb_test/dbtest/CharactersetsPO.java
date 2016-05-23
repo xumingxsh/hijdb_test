ce4715ad5083b1114b50f8990da25e8e923c6935
XuminRong <xumingxsh21@126.com>
2016-05-23 16:15:19 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 初始化

---
+++ src/hijdb_test/dbtest/CharactersetsPO.java
@@ -0,0 +1,81 @@
+package hijdb_test.dbtest;
+
+public class CharactersetsPO {
+    /**
+     * CHARACTER_SET_NAME
+     */
+    private String cHARACTER_SET_NAME;
+		
+    /**
+     * DEFAULT_COLLATE_NAME
+     */
+    private String dEFAULT_COLLATE_NAME;
+		
+    /**
+     * DESCRIPTION
+     */
+    private String dESCRIPTION;
+		
+    /**
+     * MAXLEN
+     */
+    private long mAXLEN;
+		
+
+    /**
+     * CHARACTER_SET_NAME
+    */
+    public String getCHARACTER_SET_NAME() {
+        return this.cHARACTER_SET_NAME;
+    }		
+		
+    /**
+     * CHARACTER_SET_NAME
+     */
+    public void setCHARACTER_SET_NAME(String cHARACTER_SET_NAME) {
+        this.cHARACTER_SET_NAME = cHARACTER_SET_NAME;
+    }
+	 
+    /**
+     * DEFAULT_COLLATE_NAME
+    */
+    public String getDEFAULT_COLLATE_NAME() {
+        return this.dEFAULT_COLLATE_NAME;
+    }		
+		
+    /**
+     * DEFAULT_COLLATE_NAME
+     */
+    public void setDEFAULT_COLLATE_NAME(String dEFAULT_COLLATE_NAME) {
+        this.dEFAULT_COLLATE_NAME = dEFAULT_COLLATE_NAME;
+    }
+	 
+    /**
+     * DESCRIPTION
+    */
+    public String getDESCRIPTION() {
+        return this.dESCRIPTION;
+    }		
+		
+    /**
+     * DESCRIPTION
+     */
+    public void setDESCRIPTION(String dESCRIPTION) {
+        this.dESCRIPTION = dESCRIPTION;
+    }
+	 
+    /**
+     * MAXLEN
+    */
+    public long getMAXLEN() {
+        return this.mAXLEN;
+    }		
+		
+    /**
+     * MAXLEN
+     */
+    public void setMAXLEN(long mAXLEN) {
+        this.mAXLEN = mAXLEN;
+    }
+
+}
---
+++ src/hijdb_test/dbtest/TestDBHelper.java
@@ -0,0 +1,148 @@
+package hijdb_test.dbtest;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import HiJDB.DBHelper;
+import HiJDB.DBOperate;
+import HiJDB.ICreator;
+import HiJDB.Creator.MySQLCreator;
+import HiJUtil.Generic.IEventRet;
+import HiJUtil.Generic.IEventRet8Param;
+import HiJUtil.Generic.IResult;
+
+public class TestDBHelper {
+	String url = "jdbc:mysql://localhost:3306/information_schema";
+	public TestDBHelper() {
+		DBOperate.AddDBCreator(DBOperate.MySQL, new IEventRet<ICreator>(){
+
+			@Override
+			public ICreator OnEvent() {
+				// TODO Auto-generated method stub
+				return new MySQLCreator();
+			}
+			
+		});
+	}
+	@Test
+	public void Test_DBHelper_ExecuteScalar_String() {
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+        String obj;
+		try {
+			obj = db.ExecuteScalar(String.class, "select table_name from tables limit 1");
+	        Assert.assertEquals(obj, "CHARACTER_SETS");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void Test_DBHelper_ExecuteScalar_int() {
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+        int obj;
+		try {
+			obj = db.ExecuteScalar(int.class, "select Count(1) from tables limit 1");
+	        Assert.assertTrue(obj > 0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void Test_DBHelper_ExecuteScalar_Integer() {
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+        Integer obj;
+		try {
+			obj = db.ExecuteScalar(int.class, "select Count(1) from tables limit 1");
+	        Assert.assertTrue(obj > 0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void Test_DBHelper_ExecuteScalar_String2() {
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+        int obj;
+		try {
+			obj = db.ExecuteScalar(int.class, "select table_name from tables where 1 != 1");
+	        Assert.assertEquals(obj, -1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void Test_DBHelper_ExecuteScalar_int_null() {
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+        String obj;
+		try {
+			obj = db.ExecuteScalar(String.class, "select Count(1) from tables limit 1");
+	        Assert.assertTrue(Integer.parseInt(obj) > 0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void Test_DBHelper_ExecuteQuery() {
+
+		DBOperate db = new DBOperate(url, "root", "root", DBOperate.MySQL);
+		try {
+			final IResult<List<CharactersetsPO>> result = new IResult<List<CharactersetsPO>>(){
+
+				@Override
+				public boolean GetIsSuccess() {
+					// TODO Auto-generated method stub
+					return false;
+				}
+
+				@Override
+				public List<CharactersetsPO> Get() {
+					// TODO Auto-generated method stub
+					return lst;
+				}
+				
+				List<CharactersetsPO> lst;
+
+				@Override
+				public void Set(List<CharactersetsPO> t) {
+					lst = t;
+				}
+
+				@Override
+				public boolean SetIsSuccess() {
+					// TODO Auto-generated method stub
+					return false;
+				}
+			};
+			boolean ret = db.ExecuteQuery("SELECT  CHARACTER_SET_NAME,  DEFAULT_COLLATE_NAME,  DESCRIPTION,  MAXLEN FROM Character_sets ", new IEventRet8Param<Boolean, ResultSet>(){
+
+				@Override
+				public Boolean OnEvent(ResultSet v) {
+					List<CharactersetsPO> lst = null;
+					try {
+						lst = DBHelper.GetResultsList(CharactersetsPO.class, v);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return false;
+					}
+					result.Set(lst);
+					return lst != null;
+				}
+				
+			});
+			Assert.assertTrue(result.Get() != null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
